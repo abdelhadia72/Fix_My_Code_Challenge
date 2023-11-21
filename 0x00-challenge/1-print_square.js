6982d936831e5abf6e2214e1bbd509e3083fe953
abdelhadia72 <thisisavnb@gmail.com>
2023-11-21 16:33:48 +0100

    fix js 0.7

--- 0x00-challenge/1-print_square.js
+++ 0x00-challenge/1-print_square.js
@@ -16,8 +16,8 @@ if (process.argv.length <= 2) {
 
 let size = parseInt(process.argv[2]);
 
-for (let i = 0 ; i < size ; i ++) {
-    for (let j = 0 ; j < size ; j ++) {
+for (let i = 0 ; i < size ; i++) {
+    for (let j = 0 ; j < size ; j++) {
         process.stdout.write("#");
     }
     process.stdout.write("\n");
