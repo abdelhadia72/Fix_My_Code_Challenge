e34d20865677b26855f12f186fe139230392ccf0
abdelhadia72 <thisisavnb@gmail.com>
2024-01-19 19:21:17 +0100

    Task 2 (user fixing )

--- 0x01-Fix_My_Code_Challenge/square.py
+++ 0x01-Fix_My_Code_Challenge/square.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-class square():
+class Square():
     
     width = 0
     height = 0
@@ -12,9 +12,9 @@ class square():
 
     def area_of_my_square(self):
         """ Area of the square """
-        return self.width * self.width
+        return self.width * self.height
 
-    def PermiterOfMySquare(self):
+    def PerimeterOfMySquare(self):
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
@@ -25,4 +25,4 @@ if __name__ == "__main__":
     s = square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
-    print(s.PermiterOfMySquare())
+    print(s.PerimeterOfMySquare())
--- 0x01-Fix_My_Code_Challenge/status_server/api/v1/app.py
+++ 0x01-Fix_My_Code_Challenge/status_server/api/v1/app.py
@@ -5,14 +5,14 @@ Web server
 from api.v1.views import app_views
 from flask import Flask, jsonify, make_response
 
-app = Flask(__name__)
-app.register_blueprint(app_views)
+def create_app():
+    app = Flask(__name__)
+    app.register_blueprint(app_views)
 
-
-@app.errorhandler(404)
-def not_found(error):
-    """ json 404 page """
-    return make_response(jsonify({"error": "Not found"}), 404)
+    @app.errorhandler(404)
+    def not_found(error):
+        """ json 404 page """
+        return make_response(jsonify({"error": "Not found"}), 404)
 
 
 if __name__ == "__main__":
