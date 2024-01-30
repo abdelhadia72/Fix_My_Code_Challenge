dc3c2f3e0f372d009ccdd949999ec573897ee908
abdelhadia72 <thisisavnb@gmail.com>
2024-01-30 22:06:58 +0100

    square

--- 0x01-Fix_My_Code_Challenge/square.py
+++ 0x01-Fix_My_Code_Challenge/square.py
@@ -22,7 +22,7 @@ class Square():
 
 if __name__ == "__main__":
 
-    s = square(width=12, height=9)
+    s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
     print(s.PerimeterOfMySquare())
